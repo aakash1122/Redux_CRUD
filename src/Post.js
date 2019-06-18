8cd1f38febbcab4931f05d005ac68b943f8bce17
aakash1122 <aakaashhossain@gmail.com>
2019-06-18 20:46:37 +0600

    working redux crud operation

---
+++ src/Post.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Post extends Component {
+  render() {
+    return (
+      <div>
+        <h2>{this.props.post.title}</h2>
+        <p>{this.props.post.message}</p>
+        <button
+          onClick={() =>
+            this.props.dispatch({ type: "EDIT_POST", id: this.props.post.id })
+          }
+        >
+          Edit
+        </button>
+        <button
+          onClick={() =>
+            this.props.dispatch({ type: "DELETE_POST", id: this.props.post.id })
+          }
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+}
+export default connect()(Post);
